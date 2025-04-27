b857aa20f7f45dad1964595f0f224d08b6ae56c0
d4t4mur <Niklas.Treichel@t-online.de>
2025-04-27 12:48:23 +0200

    out commentation of osm-function update of readme-file

--- R/get_osm_data.R
+++ R/get_osm_data.R
@@ -1,60 +1,62 @@
-#' @export
-
-get_osm_buildings <- function(boundary_polygon) {
-  library(osmdata)
-  library(sf)
-
-  if (!inherits(boundary_polygon, "sf")) stop("Boundary has to be a sf-object.")
-
-  # creating bounding box from boundary polygon for osm query
-  bbox <- st_bbox(boundary_polygon)
-
-  # osm query for buildings
-  osm_query <- opq(bbox = bbox) %>% add_osm_feature(key = "building")
-
-  osm_data <- osmdata_sf(osm_query)
-
-  # extract polygons and combine data types
-  buildings <- rbind(
-    osm_data$osm_polygons,
-    st_cast(osm_data$osm_multipolygons, "POLYGON")
-  )
-
-  # drop of all buildings outside the boundary polygon but within bounding box
-  buildings <- st_intersection(buildings, boundary_polygon)
-
-  return(buildings)
-}
+# for later usage to compare wsf with osm, but only for very high resolution
+# and small areas
 
 
 
 
 
-get_osm_nature <- function(boundary_polygon) {
-  library(osmdata)
-  library(sf)
-
-  if (!inherits(boundary_polygon, "sf")) stop("Boundary has to be a sf-object.")
-
-  # creating bounding box from boundary polygon for osm query
-  bbox <- st_bbox(boundary_polygon)
-
-  # osm query for natural area
-  osm_query <- opq(bbox = bbox) %>% add_osm_feature(key = "natural")
-
-  osm_data <- osmdata_sf(osm_query)
-
-  # extract polygons and combine data types
-  nature <- rbind(
-    osm_data$osm_polygons,
-    st_cast(osm_data$osm_multipolygons, "POLYGON")
-  )
-
-  # drop of all buildings outside the boundary polygon but within bounding box
-  nature <- st_intersection(nature, boundary_polygon)
-
-  return(nature)
-}
-
-
-# more filter/making separate tiles
+# get_osm_buildings <- function(boundary_polygon) {
+#   library(osmdata)
+#   library(sf)
+#
+#   if (!inherits(boundary_polygon, "sf")) stop("Boundary has to be a sf-object.")
+#
+#   # creating bounding box from boundary polygon for osm query
+#   bbox <- st_bbox(boundary_polygon)
+#
+#   # osm query for buildings
+#   osm_query <- opq(bbox = bbox) %>% add_osm_feature(key = "building")
+#
+#   osm_data <- osmdata_sf(osm_query)
+#
+#   # extract polygons and combine data types
+#   buildings <- rbind(
+#     osm_data$osm_polygons,
+#     st_cast(osm_data$osm_multipolygons, "POLYGON")
+#   )
+#
+#   # drop of all buildings outside the boundary polygon but within bounding box
+#   buildings <- st_intersection(buildings, boundary_polygon)
+#
+#   return(buildings)
+# }
+#
+#
+#
+#
+#
+# get_osm_nature <- function(boundary_polygon) {
+#   library(osmdata)
+#   library(sf)
+#
+#   if (!inherits(boundary_polygon, "sf")) stop("Boundary has to be a sf-object.")
+#
+#   # creating bounding box from boundary polygon for osm query
+#   bbox <- st_bbox(boundary_polygon)
+#
+#   # osm query for natural area
+#   osm_query <- opq(bbox = bbox) %>% add_osm_feature(key = "natural")
+#
+#   osm_data <- osmdata_sf(osm_query)
+#
+#   # extract polygons and combine data types
+#   nature <- rbind(
+#     osm_data$osm_polygons,
+#     st_cast(osm_data$osm_multipolygons, "POLYGON")
+#   )
+#
+#   # drop of all buildings outside the boundary polygon but within bounding box
+#   nature <- st_intersection(nature, boundary_polygon)
+#
+#   return(nature)
+# }
--- Readme.md
+++ Readme.md
@@ -1,24 +1,24 @@
 # WSF-CoRRelatoR
 
-The package provides functions to quickly calculate and display the correlation between any given raster dataset with a single variable and the world settlement footprint.
+This package provides functions to quickly calculate and display the correlation between the mean value of any given raster dataset with a single variable and the world settlement footprint.
 
 
 ## Conditions
 
-need to have a raster with existing coordinate reference system.
-need to download the matching world settlement footprint tile on "https://download.geoservice.dlr.de/WSF2019/"
-
-
-
+For using this package in the best way, you need to have a raster (or multiple) with an existing coordinate reference system and a single value per pixel where the mean value can be extracted.
+Furthermore the different areas to compare should be available in an vector format (polygon).
+For the world settlement footprint you can download the matching tile on "https://download.geoservice.dlr.de/WSF2019/".
 
 
 ## Dependencies
 
+The package uses the following packages:
 - `terra`
 - `sf`
 - `dplyr`
 - `ggplot2`
 - `ggrepel`
+
 
 ## Installation
 
@@ -28,16 +28,24 @@ You can simply install the package via
 library(devtools)
 install_github("D4t4mur/WSF-CoRRelatoR")
 library(Rpollution)
+```
 
 
-´´´
 ## Functions
+
+The following functions are built in so far in this package:
+
+- 'load_example_data()': loads examplary data, a summer-mean land surface temperature raster for Berlin, the wsf raster tile for Berlin and the twelve districts of Berlin as a shp-file
+- 'rastmeancalc()': extracts all raster pixel values and calculates the mean
+- 'wsfmeancalc()': extracts all wsf raster pixel values and calculates the built-up area / imperviousness in %
+- 'builtuparea()': masks the examined polygon with the built-up area from the wsf raster
+- 'combine()': combines the extracted values from data raster, wsf raster in a data frame
+- 'visual_correlation()': calculates the correlation and displays a plot showcasing the result
+
+
+## Example
+
+To load the examplary data, execute:
 
 ```r
 load_example_data()
-rastmeancalc()
-wsfmeancalc()
-builtuparea()
-
-combine()
-visual_correlation()
